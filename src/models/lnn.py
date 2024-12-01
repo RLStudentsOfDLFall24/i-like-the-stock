be0c18ac10ffb3725582df06444638acc80e4b11
killyosaur <killyosaur@gmail.com>
2024-12-01 11:49:41 -0500

    still working out my issues

--- src/models/lnn.py
+++ src/models/lnn.py
@@ -3,33 +3,39 @@ from torch import nn
 from .abstract_model import AbstractModel
 
 
+def get_activation_function(activation):
+    match activation:
+        case 'relu':
+            return nn.ReLU()
+        case 'leaky_relu':
+            return nn.LeakyReLU()
+        case 'tanh':
+            return nn.Tanh()
+        case 'sigmoid':
+            return nn.Sigmoid()
+        case 'elu':
+            return nn.ELU()
+        case 'gelu':
+            return nn.GELU()
+        case _:
+            raise Exception(f'{activation} is not currently supported')
+
+
 class LNN(AbstractModel):
     def __init__(self, batch_size, input_size, hidden_size, output_size, n_layers=6, activation='relu'):
         super(LNN, self).__init__(batch_size=batch_size)
         self.step_size = 1 / n_layers
 
-        if activation == 'relu':
-            self.activation = nn.ReLU()
-        elif activation == 'leaky_relu':
-            self.activation = nn.LeakyReLU()
-        elif activation == 'tanh':
-            self.activation = nn.Tanh()
-        elif activation == 'sigmoid':
-            self.activation = nn.Sigmoid()
-        elif activation == 'elu':
-            self.activation = nn.ELU()
-        elif activation == 'gelu':
-            self.activation = nn.GELU()
-        else:
-            raise Exception(f'{activation} is not currently supported')
+        self.activation = get_activation_function(activation)
+
         self.hidden_size = hidden_size
         self.input_size = input_size
         self.output_size = output_size
         self.n_layers = n_layers
 
         self.time_constant = nn.Parameter(torch.ones(hidden_size))
-        self.weight = nn.Parameter(torch.zeros(input_size, hidden_size))
-        self.r_weight = nn.Parameter(torch.zeros(hidden_size, hidden_size))
+        self.weight = nn.Parameter(torch.randn(input_size, hidden_size))
+        self.r_weight = nn.Parameter(torch.randn(hidden_size, hidden_size))
         self.bias = nn.Parameter(torch.zeros(hidden_size))
         self.A = nn.Parameter(torch.ones(hidden_size))
 
@@ -40,13 +46,14 @@ class LNN(AbstractModel):
         
 
     def forward(self, data: torch.Tensor):
-        N, T, _ = data.size()
-        x = torch.zeros((N, self.hidden_size)).to(self.bias.device)
-        for idx in range(T):
-            for _ in range(self.n_layers):
-                x = self.__fused_step(data[:, idx, :], x)
-        
-        return self.output(x)
+        with torch.enable_grad():
+            N, T, _ = data.size()
+            x = torch.zeros((N, self.hidden_size)).to(self.bias.device)
+            for idx in range(T):
+                for _ in range(self.n_layers):
+                    x = self.__fused_step(data[:, idx, :], x)
+            
+            return self.output(x)
     
     def __fused_step(self, data: torch.Tensor, hidden: torch.Tensor):
         func_data = data @ self.weight + hidden @ self.r_weight + self.bias.unsqueeze(0)
@@ -54,3 +61,19 @@ class LNN(AbstractModel):
         activation = self.activation(func_data)
 
         return (hidden + self.step_size * activation * self.A) / (1 + self.step_size * (1 / self.time_constant) + activation)
+
+
+from ncps.torch import LTC
+from ncps.wirings import AutoNCP 
+
+class LNN_2(AbstractModel):
+    def __init__(self, batch_size, input_size, hidden_size, output_size, n_layers=6, use_mixed=False):
+        super(LNN_2, self).__init__(batch_size=batch_size)
+        wiring = AutoNCP(hidden_size, output_size)
+        
+        self.model = LTC(input_size, wiring, ode_unfolds=n_layers, mixed_memory=use_mixed, return_sequences=False)
+
+    def forward(self, x):
+        result = self.model(x)
+
+        return result[0]
