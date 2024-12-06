cf0c2577348138180fba72e3066a431c4225952f
Michael Walker <michael.walker@wcgclinical.com>
2024-12-06 11:57:15 -0500

    baselines

--- src/models/baselines.py
+++ src/models/baselines.py
@@ -14,6 +14,8 @@ class RiddlerModel(AbstractModel):
     def __init__(self, d_features: int, device: torch.device, buy_percent=0.25, sell_percent=0.70, hold_percent=0.05, **kwargs):
         super(RiddlerModel, self).__init__(d_features, device)
 
+        self.rng = np.random.default_rng(kwargs['seed'] if 'seed' in kwargs else None)
+
         self.these_parameters_do_nothing = torch.nn.Parameter(torch.ones((84, 84)), requires_grad=True)
 
         self.percents = torch.Tensor([buy_percent, hold_percent, sell_percent]).to(self.device)
@@ -26,7 +28,7 @@ class RiddlerModel(AbstractModel):
 
         output = torch.ones((N, 3)) * .42
 
-        rand_ind = np.random.choice(3, N, p=self.percents.numpy())
+        rand_ind = self.rng.choice(3, N, p=self.percents.numpy())
         output[torch.arange(N), rand_ind] *= 4
 
         return output.to(self.device)
